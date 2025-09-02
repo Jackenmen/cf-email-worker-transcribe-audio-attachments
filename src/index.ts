8d13bc97b53bb603e11f9f4f4657dc3424fd9f8f
Jakub Kuczys <me@jacken.men>
2025-09-02 12:24:15 +0200

    Fix type issue

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,9 @@ export default {
 		let addedPlain = '\nTranscription of attachments below:';
 		let addedHtml = '<p>Transcription of attachments below:</p>';
 		for (const attachment of email.attachments) {
+			if (typeof attachment.content === "string") {
+				continue;
+			}
 			const blob = attachment.content;
 			const input = {audio: [...new Uint8Array(blob)]};
 			const response = await env.AI.run('@cf/openai/whisper', input);
